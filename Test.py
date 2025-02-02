75e2db9923f19e5adcc9782832adae020f4ad475
Tsunami014 (Max) <Tsunami014@duck.com>
2025-02-02 13:17:53 +1100

    Gave Test.py some functionality

--- Test.py
+++ Test.py
@@ -5,3 +5,17 @@ Author: Tsunami014
 Email: tsunami014@duck.com
 Version: 0.1.0
 """
+
+from API import App, Popup, StaticPos, RelativePos
+import widgets as wids
+import bar
+
+class Test(App):
+    def init_widgets(self):
+        return [
+            wids.Text(StaticPos(0, 0), 'Hello, World!'), 
+            wids.Button(StaticPos(0, 1), 'Click me!', lambda: Popup(wids.Text(StaticPos(0, 0), 'This is a popup!\nHi!'))),
+            wids.TextInput(RelativePos(1, 0, len('Hello, World! '), 0), placeholder='Type here: ')
+        ]
+
+bar.BarApp(Test)
